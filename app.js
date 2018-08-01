2ed3a94ca3e9cbe55591419d02899930149be330
Vedrana <vedrana_ravlic16@yahoo.com>
2018-08-01 10:55:38 +0200

    Added jQuery styling to the form element

---
+++ app.js
@@ -0,0 +1,18 @@
+$('input').focus(function(){
+	$(this).css('background', '#cff700');
+});
+
+$('textarea').focus(function(){
+	$(this).css('background', '#cff700');
+});
+
+$('input').blur(function(){
+	$(this).css('background', '#f4f4f4');
+});
+
+$('textarea').blur(function(){
+	$(this).css('background', '#f4f4f4');
+});
+
+
+
